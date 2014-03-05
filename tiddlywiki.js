10883a43af181eff45aed40260f5c6ab059316ba
Osvaldo Zonetti <zonetti@users.noreply.github.com>
2014-03-05 18:06:57 -0300

    start using basic auth

--- tiddlywiki.js
+++ tiddlywiki.js
@@ -7,7 +7,16 @@ This is invoked as a shell script by NPM when the `tiddlywiki` command is typed
 var $tw = require("./boot/boot.js").TiddlyWiki();
 
 // Pass the command line arguments to the boot kernel
-$tw.boot.argv = Array.prototype.slice.call(process.argv,2);
+$tw.boot.argv = [
+  'editions/zonetti',
+  '--server',
+  '3000',
+  '$:/core/save/all',
+  'text/plain',
+  'text/html',
+  'user',
+  'password'
+];
 
 // Boot the TW5 app
 $tw.boot.boot();
